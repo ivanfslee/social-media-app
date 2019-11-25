97d4a0708d77add91fd6bd5178d97b01a38f6a5f
ivanfslee <ivanfslee@gmail.com>
2019-11-25 15:44:52 -0800

    Implemented webpack to bundle frontend javascript

---
+++ frontend-js/main.js
@@ -0,0 +1,5 @@
+import Search from './modules/search' 
+//not using 'require' syntax because babel is transpiling newer 'import' syntax 
+//node is still using older syntax 'require'
+
+new Search();
